bf379534eea72a0c85e180edd434e3ee5d3a73b1
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-01-14 14:59:29 +0100

    le hook d'état-useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import ClassState from './components/ClassState';
-import FunctionState from './components/FunctionState';
+import React from 'react';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
-      <h1 className="text-center">useState Hooks</h1>
-
-      <ClassState />
-      <hr />
-      <FunctionState />
+      <Todo />
     </div>
   );
 }
